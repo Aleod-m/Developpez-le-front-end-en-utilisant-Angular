7f3a49edd5eb37567ce43b674817a30d9b04590e
AdrienDML <adriendml99@protonmail.com>
2024-12-05 08:52:12 +0100

    Types for Olympic Contries and remove all any types.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { OlympicCountry } from 'src/app/core/models/OlympicCountry';
+import { BoxedTextComponent } from 'src/app/core/components/BoxedTextComponent';
+import { RowComponent } from 'src/app/core/components/RowComponent';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +11,7 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Array<OlympicCountry>> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
