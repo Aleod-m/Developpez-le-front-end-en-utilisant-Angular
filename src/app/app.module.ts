e06a2da47bf716052a43f491e3612f84f6b18bce
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-12-06 10:34:00 +0100

    Load ngx-charts module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { BoxedTextComponent } from 'src/app/core/components/BoxedTextComponent';
+import { RowComponent } from 'src/app/core/components/RowComponent';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent, HomeComponent, NotFoundComponent, BoxedTextComponent, RowComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
