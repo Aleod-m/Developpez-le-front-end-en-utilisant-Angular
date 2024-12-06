9ecc60215a55f07699c5fb73454cafaaa52447f0
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-12-06 10:59:37 +0100

    Add event listener on chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, Subscription, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { OlympicCountry } from 'src/app/core/models/OlympicCountry';
 import { BoxedTextComponent } from 'src/app/core/components/BoxedTextComponent';
@@ -12,6 +12,7 @@ import { RowComponent } from 'src/app/core/components/RowComponent';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Array<OlympicCountry>> = of([]);
+  olympics_subscription: Subscription | null = null;
   // Chart settings.
   data: Array<{name: string, value: number}> = [];
   show_labels: boolean = true;
@@ -24,7 +25,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-    this.olympics$.subscribe((countries: Array<OlympicCountry>) => {
+    this.olympics_subscription = this.olympics$.subscribe((countries: Array<OlympicCountry>) => {
       this.data = countries.map((country: OlympicCountry) => {
         let total_medal_count = country.participations.reduce((tot, part) => tot + part.medalsCount, 0);
         return {
@@ -33,5 +34,16 @@ export class HomeComponent implements OnInit {
         };
       });
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.olympics_subscription != null) {
+      this.olympics_subscription.unsubscribe()
+    }
+  }
+
+  onChartSelect(data: {name: string, value: number}): void {
+    console.log("name: ", data.name);
+    console.log("value: ", data.value);
   }
 }
