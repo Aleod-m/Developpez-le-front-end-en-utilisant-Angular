dbd02352d88aa3c877be4e76b36f2a9269f3b7d1
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-12-06 10:47:33 +0100

    Home pie chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,10 +12,26 @@ import { RowComponent } from 'src/app/core/components/RowComponent';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Array<OlympicCountry>> = of([]);
+  // Chart settings.
+  data: Array<{name: string, value: number}> = [];
+  show_labels: boolean = true;
+  trim_labels: boolean = false;
+  show_legend: boolean = true;
+  legend_position: string = 'below';
+
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.subscribe((countries: Array<OlympicCountry>) => {
+      this.data = countries.map((country: OlympicCountry) => {
+        let total_medal_count = country.participations.reduce((tot, part) => tot + part.medalsCount, 0);
+        return {
+          name: country.country,
+          value:  total_medal_count,
+        };
+      });
+    });
   }
 }
