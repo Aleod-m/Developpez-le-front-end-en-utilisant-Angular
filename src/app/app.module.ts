b0f88783b1f11b8870563f5926c01c79858ed8ec
Adrien Derobert-Mazure <adrien.derobertmazure@biblibre.com>
2024-12-06 13:35:29 +0100

    Reformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { BoxedTextComponent } from 'src/app/core/components/BoxedTextComponent';
 import { RowComponent } from 'src/app/core/components/RowComponent';
@@ -11,9 +13,20 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, NotFoundComponent, BoxedTextComponent, RowComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    NotFoundComponent,
+    BoxedTextComponent,
+    RowComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxChartsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
